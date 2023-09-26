26b0568ba7a27878c9e81342e77e9c2da3e4d15c
Timmy Jose <zoltan.jose@gmail.com>
2023-09-26 11:02:55 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import { extendTheme } from '@chakra-ui/react'
+
+// custom theme
+export const colors = {
+    // new values
+    brand: {
+        900: '#024fc9',
+        800: '#145af5',
+        700: '#2977f2',
+        600: '#337df2',
+        500: '#4287f5'
+    }
+}
+
+export const fonts = {
+    // update existing values
+    body: 'Tahoma',
+    heading: 'Courier New'
+}
+
+const dojoTasksTheme = extendTheme({ colors, fonts })
+
+export default dojoTasksTheme
